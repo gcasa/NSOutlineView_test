c9dbade2b20c52d62f464355760b40657bfeeedb
Gregory John Casamento <greg.casamento@gmail.com>
2023-04-16 23:21:11 -0400

    Update data source

--- NSOutlineView_test/AppDelegate.m
+++ NSOutlineView_test/AppDelegate.m
@@ -28,5 +28,49 @@
     return YES;
 }
 
+// Outline view data source
+- (NSInteger) outlineView:(NSOutlineView *)outlineView numberOfChildrenOfItem:(nullable id)item
+{
+    if (item == nil)
+        return 1;
+    else if ([item isEqualToString: @"Item 0"])
+        return 1;
+    else if ([item isEqualToString: @"Item 1"])
+        return 0;
+    
+    return 0;
+}
+
+- (id) outlineView:(NSOutlineView *)outlineView
+             child:(NSInteger)index
+            ofItem:(nullable id)item
+{
+    if (index == 0 && item == nil)
+    {
+        return @"Item 0";
+    }
+    else if (index == 0 && [item isEqualToString:@"Item 0"])
+    {
+        return @"Item 1";
+    }
+    
+    return nil;
+}
+
+- (BOOL) outlineView:(NSOutlineView *)outlineView
+    isItemExpandable:(id)item
+{
+    if ([item isEqualToString: @"Item 0"])
+        return YES;
+    
+    return NO;
+}
+
+- (id)outlineView:(NSOutlineView *)outlineView objectValueForTableColumn:(NSTableColumn *)tableColumn byItem:(id)item
+{
+    return item;
+}
+
+// Outline view delegate
 
 @end
